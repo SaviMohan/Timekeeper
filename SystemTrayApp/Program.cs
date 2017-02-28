7b4ee16e4b6f4a2f28f367f2a9f3508d3496195e
Mark Yep <mark_yep@hotmail.com>
2017-03-01 11:26:56 +1300

    finished detection software

--- SystemTrayApp/Program.cs
+++ SystemTrayApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Timers;
 using System.Windows.Forms;
 
@@ -43,8 +45,26 @@ namespace SystemTrayApp
 
         private static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine("Hello World!");
-            MessageBox.Show("The calculations are complete", "My Application", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            string title = GetActiveWindowTitle();
+            MessageBox.Show(title, "My Application", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return "Nothing Detected";
         }
     }
 }
