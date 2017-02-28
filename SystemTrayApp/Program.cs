acc6d394f357afc13a8007e3eebe769875b6de80
Mark Yep <mark_yep@hotmail.com>
2017-03-01 11:18:27 +1300

    system tray app

---
+++ SystemTrayApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace SystemTrayApp
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Show the system tray icon.					
+            using (ProcessIcon pi = new ProcessIcon())
+			{
+				pi.Display();
+
+                // Make sure the application runs!
+                Application.Run(new Sync());
+            }
+        }
+    }
+
+    class Sync : ApplicationContext
+    {
+        public Sync()
+        {
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
+
+            System.Diagnostics.Debug.WriteLine("Timer started");
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello World!");
+            MessageBox.Show("The calculations are complete", "My Application", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+        }
+    }
+}
--- SystemTrayApp/Properties/AssemblyInfo.cs
+++ SystemTrayApp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsTimeService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Timekeeper Client")]
+[assembly: AssemblyDescription("Timekeeper Client System Tray Application.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsTimeService")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("CELL")]
+[assembly: AssemblyProduct("Timekeeper")]
+[assembly: AssemblyCopyright("Copyright © 2017. All Rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9e286b56-8cb2-4b8a-a976-b59a0c8bdb41")]
+[assembly: Guid("116c8741-a9b0-4560-8e82-7cf412894340")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
