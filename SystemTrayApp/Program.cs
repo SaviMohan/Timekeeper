a0c42c74557f12e15a0817cf8296602b97e399d0
Mark Yep <mark_yep@hotmail.com>
2017-03-02 11:58:16 +1300

    updated wth mysql

--- SystemTrayApp/Program.cs
+++ SystemTrayApp/Program.cs
@@ -3,6 +3,8 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Timers;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 
 namespace SystemTrayApp
 {
@@ -33,6 +35,9 @@ namespace SystemTrayApp
 
     class Sync : ApplicationContext
     {
+
+        private List<string> titles = new List<string>();
+
         public Sync()
         {
             System.Timers.Timer aTimer = new System.Timers.Timer();
@@ -40,13 +45,25 @@ namespace SystemTrayApp
             aTimer.Interval = 5000;
             aTimer.Enabled = true;
 
-            System.Diagnostics.Debug.WriteLine("Timer started");
+            System.Timers.Timer bTimer = new System.Timers.Timer();
+            bTimer.Elapsed += new ElapsedEventHandler(OnTimedEventTwo);
+            bTimer.Interval = 30000;
+            bTimer.Enabled = true;      
+
+            System.Diagnostics.Debug.WriteLine("Timers started");
+       
         }
 
-        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
         {
             string title = GetActiveWindowTitle();
-            MessageBox.Show(title, "My Application", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            System.Diagnostics.Debug.WriteLine(title);
+            titles.Add(title);
+        }
+
+        private void OnTimedEventTwo(object source, ElapsedEventArgs e)
+        {
+            updateDB();
         }
 
         [DllImport("user32.dll")]
@@ -66,5 +83,40 @@ namespace SystemTrayApp
             }
             return "Nothing Detected";
         }
+
+        private void updateDB()
+        {
+            string constring = "Database=timekeeperdata;Data Source=au-cdbr-azure-southeast-a.cloudapp.net;User Id=b36cbbf06103db;Password=3be84fed";
+
+            int len = titles.Count;
+            for (int i=0;i<len;i++)
+            {
+                string query = "INSERT INTO timekeeperdata.test2(test_titles) VALUES('" + titles[0] + "');";
+                MySqlConnection conDatabase = new MySqlConnection(constring);
+                MySqlCommand cmdDatabase = new MySqlCommand(query, conDatabase);
+                MySqlDataReader myReader;
+
+                try
+                {
+                    conDatabase.Open();
+                    myReader = cmdDatabase.ExecuteReader();
+                    System.Diagnostics.Debug.WriteLine("SAVED");
+                    while (myReader.Read())
+                    {
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+
+                titles.RemoveAt(0);        
+                cmdDatabase.Connection.Close();
+                conDatabase.Close();
+            }
+            
+        }
+
     }
 }
