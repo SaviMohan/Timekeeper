2ed70c0d3b7674697bb5d2b3204c06615e3c133c
Mark Yep <mark_yep@hotmail.com>
2017-03-01 13:07:40 +1300

    trying to incorporate easytables

---
+++ SystemTrayApp/AzureManager.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using SystemTrayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SystemTrayApp
+{
+    public class AzureManager
+    {
+        private static AzureManager instance;
+        private MobileServiceClient client;
+        private IMobileServiceTable<moneyTable> currencyAccount;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("http://msaeasytables.azurewebsites.net");
+            this.currencyAccount = this.client.GetTable<moneyTable>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+
+        public async Task DeleteAccount(moneyTable account)
+        {
+            await currencyAccount.DeleteAsync(account);
+        }
+
+
+        public async Task CreateAccount(moneyTable account)
+        {
+            await this.currencyAccount.InsertAsync(account);
+        }
+
+        public async Task<List<moneyTable>> GetAccount()
+        {
+            return await this.currencyAccount.ToListAsync();
+        }
+
+        public async Task UpdateAccount(moneyTable account)
+        {
+            await this.currencyAccount.UpdateAsync(account);
+        }
+    }
+}
---
+++ SystemTrayApp/Models/DataModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SystemTrayApp.Models
+{
+    public class moneyTable
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty(PropertyName = "updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty(PropertyName = "version")]
+        public string Version { get; set; }
+
+        [JsonProperty(PropertyName = "deleted")]
+        public bool Deleted { get; set; }
+
+        [JsonProperty(PropertyName = "balance")]
+        public double Balance { get; set; }
+
+        [JsonProperty(PropertyName = "password")]
+        public int Password { get; set; }
+
+        [JsonProperty(PropertyName = "test")]
+        public string Title { get; set; }
+    }
+}
