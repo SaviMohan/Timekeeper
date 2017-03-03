c5b5244ec3dd9d47c3d39eaa07acca47726f0fda
Mark Yep <mark_yep@hotmail.com>
2017-03-04 11:08:08 +1300

    trying to get xamarin working:

---
+++ TimekeeperClientApp/TimekeeperClientApp/TimekeeperClientApp/App.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using Xamarin.Forms;
+
+namespace TimekeeperClientApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "TimekeeperClientApp",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        
+        private void updateData()
+        {        
+            try
+            {
+                string MyConnection2 = "datasource=localhost;port=3307;username=root;password=root";
+                string Query = "select * from student.studentinfo;";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MyConn2.Open(); 
+
+                //For offline connection we will use  MySqlDataAdapter class. 
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+                DataTable dTable = new DataTable();
+                MyAdapter.Fill(dTable);
+
+                //dataGridView1.DataSource = dTable; 
+                MyConn2.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+        }    
+        
+    }
+}
