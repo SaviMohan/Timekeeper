0f2502c28dfdc99c470338fba41a9b67ec389f99
Mark Yep <mark_yep@hotmail.com>
2017-03-06 19:48:29 +1300

    added User class

---
+++ TimekeeperDisplayApp/TimekeeperDisplayApp/TimekeeperDisplayApp/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimekeeperDisplayApp
+{
+    public class User
+    {
+        public string name;
+        public int userID;
+        public int companyID;
+        public List<Data> applicationLog; 
+
+        public User() 
+        {
+
+        }
+
+        public User(Data myData)
+        {
+
+        }
+
+        
+    }
+}
